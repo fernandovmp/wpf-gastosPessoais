4d21d01519a977467bf5ad0f11f542a2c4b3c22c
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-25 20:31:24 -0300

    Dependency Property de marca d'água.

---
+++ Views/Watermark.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+
+namespace wpf_gastosPessoais.Views
+{
+    public static class Watermark
+    {
+
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached(
+            "Watermark", typeof(object), typeof(Watermark), 
+            new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnWatermarkChanged)));
+
+        public static object GetWatermark(Control control)
+        {
+            return control.GetValue(WatermarkProperty);
+        }
+
+        public static  void SetWatermark(Control control, object value)
+        {
+            control.SetValue(WatermarkProperty, value);
+        }
+
+        private static void OnWatermarkChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            Control control = (Control)dependencyObject;
+            control.Loaded += ControlLoaded;
+            if(dependencyObject is TextBox)
+            {
+                control.GotFocus += ControlGotFocus;
+                control.LostFocus += ControlLoaded;
+            }
+        }
+
+        private static void ControlGotFocus(object sender, RoutedEventArgs e)
+        {
+            Control control = (Control)sender;
+            if (ShouldShowWatermark(control))
+            {
+                RemoveWatermark(control);
+            }
+        }
+
+        private static void ControlLoaded(object sender, RoutedEventArgs e)
+        {
+            Control control = (Control)sender;
+            if (ShouldShowWatermark(control))
+            {
+                ShowWatermark(control);
+            }
+        }
+
+        private static bool ShouldShowWatermark(Control control)
+        {
+            DependencyProperty dependencyProperty = GetDependencyProperty(control);
+            if (dependencyProperty == null) return true;
+            return control.GetValue(dependencyProperty).Equals("");
+        }
+
+        private static void ShowWatermark(Control control)
+        {
+            AdornerLayer layer = AdornerLayer.GetAdornerLayer(control);
+            layer.Add(new WatermarkAdorner(control, GetWatermark(control)));
+        }
+
+        private static void RemoveWatermark(UIElement control)
+        {
+            AdornerLayer layer = AdornerLayer.GetAdornerLayer(control);
+            Adorner[] adorners = layer.GetAdorners(control);
+            if (adorners == null) return;
+            foreach (Adorner adorner in adorners)
+            {
+                if(adorner is WatermarkAdorner)
+                {
+                    adorner.Visibility = Visibility.Collapsed;
+                    layer.Remove(adorner);
+                }
+            }
+        }
+
+        private static DependencyProperty GetDependencyProperty(Control control)
+        {
+            if (control is TextBoxBase)
+                return TextBox.TextProperty;
+            return null;
+        }
+
+    }
+}
---
+++ Views/WatermarkAdoner.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace wpf_gastosPessoais.Views
+{
+    public class WatermarkAdorner : Adorner
+    {
+
+        public WatermarkAdorner(UIElement adornedElement, object watermark) : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+            presenter = new ContentPresenter();
+            presenter.Opacity = 0.7;
+            presenter.Content = watermark;
+            presenter.Margin = new Thickness(Control.Margin.Left + Control.Padding.Left, 
+                Control.Margin.Top + Control.Padding.Top, 0, 0);
+        }
+
+        private readonly ContentPresenter presenter;
+
+        private Control Control
+        {
+            get { return (Control)this.AdornedElement; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return presenter;
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            presenter.Measure(Control.RenderSize);
+            return Control.RenderSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            presenter.Arrange(new Rect(finalSize));
+            return finalSize;
+        }
+    }
+
+}
+
