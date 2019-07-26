80c869e689f192921599a3b126f2a3e4a30c7907
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-26 19:39:31 -0300

    Classe que implementa interface INotifyPropertyChanged.

---
+++ Misc/NotifyPropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_gastosPessoais.Misc
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(params string[] properties)
+        {
+            foreach (var property in properties)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
