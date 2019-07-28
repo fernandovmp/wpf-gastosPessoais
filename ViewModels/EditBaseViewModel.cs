fde98ed44f6f6465d29b491351bbaacc922469bc
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-28 15:56:39 -0300

    Classe EditBaseViewModel.

---
+++ ViewModels/EditBaseViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace wpf_gastosPessoais.ViewModels
+{
+    public class EditBaseViewModel : ViewModelBase
+    {
+        protected ICommand confirm;
+        protected ICommand cancel;
+        protected bool isEditMode;
+        public ICommand Confirm
+        {
+            get
+            {
+                if (confirm == null)
+                    confirm = SelectCommand();
+                return confirm;
+            }
+            set
+            {
+                confirm = value;
+                OnPropertyChanged("Confirm");
+            }
+        }
+        public ICommand Cancel
+        {
+            get
+            {
+                if (cancel == null)
+                    cancel = new RelayCommand(CancelCommand);
+                return cancel;
+            }
+            set
+            {
+                cancel = value;
+                OnPropertyChanged("Cancel");
+            }
+        }
+        public Visibility AddVisibility
+        {
+            get => !isEditMode ? Visibility.Visible : Visibility.Collapsed;
+            
+        }
+        private ICommand SelectCommand()
+        {
+            if (isEditMode)
+                return new RelayCommand(EditCommand);
+            return new RelayCommand(AddCommand);
+        }
+
+        protected virtual void AddCommand(object parameter)
+        {
+            CancelCommand(parameter);
+        }
+
+        protected virtual void EditCommand(object parameter)
+        {
+            CancelCommand(parameter);
+        }
+
+        protected virtual void CancelCommand(object parameter)
+        {
+            ((Window)parameter).Close();
+        }
+    }
+}
