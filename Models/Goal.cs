2d8d37e6b5a91e1a500038bb0019bf8d10990ed3
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-28 15:55:48 -0300

    Goal herda de NotifyPropertyChanged

--- Models/Goal.cs
+++ Models/Goal.cs
@@ -1,19 +1,57 @@
 ﻿using Database;
+using wpf_gastosPessoais.Misc;
 
 namespace wpf_gastosPessoais.Models
 {
-    public class Goal
+    public class Goal : NotifyPropertyChanged
     {
+        private string name;
+        private decimal value;
+        private decimal savedValue;
+        private bool completed;
         [DBOption(AutoIncrement = true, PrimaryKey = true)]
         public int      Id { get; set; }
         [DBOption()]
-        public string   Name { get; set; }
+        public string   Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
         [DBOption()]
-        public decimal  Value { get; set; }
+        public decimal  Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnPropertyChanged("Value", "Progress", "IntProgress");
+            }
+        }
         [DBOption()]
-        public decimal  SavedValue { get; set; }
+        public decimal  SavedValue
+        {
+            get => savedValue;
+            set
+            {
+                savedValue = value;
+                OnPropertyChanged("SavedValue", "Progress", "IntProgress");
+            }
+        }
         public float    Progress { get => (float)(SavedValue / Value); }
+        public int      IntProgress { get => (int)(Progress * 100); }
         [DBOption()]
-        public bool     Completd { get; set; }
+        public bool     Completed
+        {
+            get => completed;
+            set
+            {
+                completed = value;
+                OnPropertyChanged("Completed");
+            }
+        }
     }
 }
