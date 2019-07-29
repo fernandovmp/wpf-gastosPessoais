10480b59861a01378739af738039677e881fe193
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-29 16:02:41 -0300

    Interface e classe para cuidar de host de janela.

---
+++ Misc/IWindowHost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_gastosPessoais.Misc
+{
+    interface IWindowHost
+    {
+        void Show(object context);
+        bool? ShowDialog(object context);
+    }
+}
---
+++ Misc/WindowHost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf_gastosPessoais.Misc
+{
+    public class WindowHost : IWindowHost
+    {
+        public WindowHost()
+        {
+            window = new WindowHostView();
+        }
+
+        protected Window window;
+
+        public virtual void Show(object context)
+        {
+            window.DataContext = context;
+            window.Show();
+        }
+
+        public virtual bool? ShowDialog(object context)
+        {
+            window.DataContext = context;
+            return window.ShowDialog();
+        }
+    }
+}
