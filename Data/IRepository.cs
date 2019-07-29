c2d4c7573e03eb78b7b95225d46ceac945b09172
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-07-29 14:51:29 -0300

    Interface IRepository.

---
+++ Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_gastosPessoais.Data
+{
+    public interface IRepository<T>
+    {
+        T[] GetAll();
+        void Save(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        int NextId { get; set; }
+    }
+}
