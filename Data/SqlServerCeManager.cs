7745e0671d6af03c4e8ba097aa92a18c535d2d71
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-08-20 21:19:49 -0300

    Classe ponte para banco de dados.

---
+++ Data/SqlServerCeManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Data.Common;
+
+namespace wpf_gastosPessoais.Data
+{
+    public class SqlServerCeManager
+    {
+        private static SqlCeConnection dbConnection;
+
+        public static void OpenConnection(string connection)
+        {
+            dbConnection = new SqlCeConnection(connection);
+            dbConnection.Open();
+        }
+
+        public string CreateTable(string name, IEnumerable<string> fields, IEnumerable<string> primaryKeys)
+        {
+            string primaryKey = "";
+            if (primaryKeys.Count() > 0)
+            {
+                primaryKey = $"primary key({string.Join(",", primaryKeys)})";
+            }
+            return $"create table {name} ({string.Join(",", fields)}, {primaryKey})";
+        }
+
+        public async void ExecuteQuerry(string querry)
+        {
+            SqlCeCommand command = new SqlCeCommand(querry, dbConnection);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task<IDataReader> ExecuteReader(string querry)
+        {
+            SqlCeCommand command = new SqlCeCommand(querry, dbConnection);
+            DbDataReader reader = await command.ExecuteReaderAsync();
+            return reader;
+        }
+
+    }
+}
